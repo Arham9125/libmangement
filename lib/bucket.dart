b274dc07be47af880ab4c538f6b4aee346160b9a
Arham Khan <arham9125@gmail.com>
2024-05-16 22:20:42 +0500

    code is updated

--- lib/bucket.dart
+++ lib/bucket.dart
@@ -3,49 +3,62 @@ import 'package:libmanagement/function/cart.dart';
 import 'package:provider/provider.dart';
 
 class Bucket extends StatelessWidget {
-  
-double price = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Consumer<CartModel>(
         builder: (context, cart, child) {
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: cart.cartitems.length,
-                  itemBuilder: (context, index) {
-                    final item = cart.cartitems[index];
-                    return ListTile(
-                      trailing:Padding(
-                        padding:  EdgeInsets.only(right: 10),
-                        child: IconButton(
-                            icon: Icon(Icons.remove_circle),
-                            onPressed: () {
-                              cart.removetocart(index); 
-                            },
-                          ),
-                      ),
-                      title: Padding(
-                        padding:  EdgeInsets.all(8.0),
-                        child: Container(
-                          height: MediaQuery.of(context).size.height*0.2,
-                          width: MediaQuery.of(context).size.width*0.2,
-                          child: Card(
-                            elevation: 2,
-                            child: Center(child: Text("${1 + index} :  ${item.title}", style: TextStyle(fontWeight: FontWeight.bold, 
-                            fontSize: 17
-                            ),))),
-                        ),
-                      ),
-                    );
-                  },
+          return ListView.builder(
+            itemCount: cart.cartitems.length,
+            itemBuilder: (context, index) {
+              final item = cart.cartitems[index];
+              return Dismissible(
+                key: Key(item.id.toString()),
+                direction: DismissDirection.endToStart,
+                onDismissed: (direction) {
+                  cart.removetocart(index);
+                },
+                background: Container(
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.only(right: 20.0),
+                  color: Colors.red,
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-             
-            ],
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100, 
+                          child: Image.network(
+                            item.thumbnail,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          item.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           );
         },
       ),
